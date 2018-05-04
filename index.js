4a6f4d670d198da1f5e1446850e175f02b95c058
jbdebiasio <jbaptistedebiasio@gmail.com>
2018-05-04 14:46:16 +0200

    version 0.2.0

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import prismic from 'prismic-javascript';
+import plugin from './src/plugin';
+
+import EditButton from './src/components/EditButton';
+import Embed from './src/components/Embed';
+import Image from './src/components/Image';
+import Link from './src/components/Link';
+import RichText from './src/components/RichText';
 
 export default {
-  install: function (Vue, options = {}) {
-    Vue.prototype.$prismic = prismic;
-    Vue.prototype.$prismic.endpoint = options.endpoint;
-  }
+  plugin,
+  EditButton,
+  Embed,
+  Image,
+  Link,
+  RichText
 };
