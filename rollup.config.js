a2561297eead3c710fc850700b6c9f695da0d966
Andre Loreth <andre.loreth@live.de>
2019-03-03 20:36:10 +0100

    Rollup: Removed vue plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 /* tslint:disable */
 import path from 'path'
 import commonjs from 'rollup-plugin-commonjs'
-import vue from 'rollup-plugin-vue'
 import babel from 'rollup-plugin-babel'
 import typescript2 from 'rollup-plugin-typescript2'
 
@@ -25,7 +24,6 @@ export default {
   plugins: [
     typescript2(),
     commonjs(),
-    vue(),
     babel({
       exclude: 'node_modules/**'
     })
