2edb6ffb17ad0ab9acc34ad59b39e5c622f83bac
Andre Loreth <andre.loreth@live.de>
2019-03-03 20:43:36 +0100

    Changed output file names to match typescript definition files

--- rollup.config.js
+++ rollup.config.js
@@ -8,15 +8,15 @@ export default {
   input: 'src/PrismicVue.ts',
   output: [
     {
-      file: path.resolve('./dist/prismic-vue.common.js'),
+      file: path.resolve('./dist/PrismicVue.common.js'),
       format: 'cjs'
     },
     {
-      file: path.resolve('./dist/prismic-vue.esm.js'),
+      file: path.resolve('./dist/PrismicVue.esm.js'),
       format: 'es'
     },
     {
-      file: path.resolve('./dist/prismic-vue.js'),
+      file: path.resolve('./dist/PrismicVue.js'),
       format: 'umd',
       name: 'PrismicVue'
     }
