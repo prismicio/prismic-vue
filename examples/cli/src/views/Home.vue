cf4703ed11fb6f573a6d85cccfd01e842165609c
lihbr <lihbr@users.noreply.github.com>
2021-03-27 13:11:51 +0100

    chore(example): vue-cli

---
+++ examples/cli/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">home</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home"
+});
+</script>
