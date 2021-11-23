4c0ef01d5a61ceace8567021658b7f6cdc438b18
lihbr <lihbr@users.noreply.github.com>
2021-11-23 12:02:05 +0100

    feat: add `<PrismicText>` component

---
+++ src/components/Text.js
@@ -0,0 +1,37 @@
+import { RichText } from "prismic-dom";
+
+export default {
+	name: "PrismicText",
+	functional: true,
+	props: {
+		field: {
+			type: Array,
+			required: true,
+		},
+		separator: {
+			type: String,
+			default: undefined,
+			required: false,
+		},
+		wrapper: {
+			type: String,
+			required: false,
+			default: "div",
+		},
+	},
+	render(h, { props, data, children, parent }) {
+		const { field, separator, wrapper } = props;
+
+		if (!field) {
+			return null;
+		}
+		const innerHTML = RichText.asText(field, separator);
+
+		return h(wrapper, {
+			...data,
+			domProps: {
+				innerHTML,
+			},
+		});
+	},
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,29 @@
-import Embed from './Embed'
-import Image from './Image'
-import Link from './Link'
-import RichText from './RichText'
+import Embed from "./Embed";
+import Image from "./Image";
+import Link from "./Link";
+import RichText from "./RichText";
+import Text from "./Text";
 
-const NuxtLink = Link({ component: 'nuxt-link' })
-const VueRouterLink = Link({ component: 'router-link' })
+const NuxtLink = Link({ component: "nuxt-link" });
+const VueRouterLink = Link({ component: "router-link" });
 
 const exp = {
-  common: {
-    Embed,
-    Image,
-    RichText,
-  },
-  nuxt: {
-    Link: NuxtLink,
-  },
-  vueRouter: {
-    Link: VueRouterLink,
-  }
-}
+	common: {
+		Embed,
+		Image,
+		RichText,
+		Text,
+	},
+	nuxt: {
+		Link: NuxtLink,
+	},
+	vueRouter: {
+		Link: VueRouterLink,
+	},
+};
 
-export const common = exp.common
-export const nuxt = exp.nuxt
-export const vueRouter = exp.vueRouter
+export const common = exp.common;
+export const nuxt = exp.nuxt;
+export const vueRouter = exp.vueRouter;
 
-export default exp
+export default exp;
