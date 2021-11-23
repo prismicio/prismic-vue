88f7f1169a96110779e41ef5a865ad9caf1c8c24
lihbr <lihbr@users.noreply.github.com>
2021-11-23 11:58:18 +0100

    feat: allow passing a link resolver to `<PrismicRichText>`

--- src/components/RichText.js
+++ src/components/RichText.js
@@ -1,45 +1,44 @@
-import { RichText } from 'prismic-dom'
+import { RichText } from "prismic-dom";
 
 export default {
-  name: 'PrismicRichText',
-  functional: true,
-  props: {
-    field: {
-      type: Array,
-      required: true,
-    },
-    htmlSerializer: {
-      type: Function,
-      required: false,
-    },
-    wrapper: {
-      type: String,
-      required: false,
-      default: 'div',
-    }
-  },
-  render(h, {
-    props, data, children, parent
-  }) {
-    const { field, htmlSerializer, wrapper } = props
+	name: "PrismicRichText",
+	functional: true,
+	props: {
+		field: {
+			type: Array,
+			required: true,
+		},
+		linkResolver: {
+			type: Function,
+			required: false,
+		},
+		htmlSerializer: {
+			type: Function,
+			required: false,
+		},
+		wrapper: {
+			type: String,
+			required: false,
+			default: "div",
+		},
+	},
+	render(h, { props, data, children, parent }) {
+		const { field, linkResolver, htmlSerializer, wrapper } = props;
 
-    if (!field) {
-      return null
-    }
-    const innerHTML = RichText.asHtml(
-      field,
-      parent.$prismic.linkResolver,
-      htmlSerializer || parent.$prismic.htmlSerializer
-    )
+		if (!field) {
+			return null;
+		}
+		const innerHTML = RichText.asHtml(
+			field,
+			linkResolver || parent.$prismic.linkResolver,
+			htmlSerializer || parent.$prismic.htmlSerializer
+		);
 
-    return h(
-      wrapper,
-      {
-        ...data,
-        domProps: {
-          innerHTML
-        },
-      },
-    )
-  }
-}
+		return h(wrapper, {
+			...data,
+			domProps: {
+				innerHTML,
+			},
+		});
+	},
+};
