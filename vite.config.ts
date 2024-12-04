c02475bad2790cd76ae2395ca2aa075d24a13546
lihbr <lihbr@users.noreply.github.com>
2024-12-04 12:01:21 +0100

    build: compile vue components

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 /// <reference types="vitest/config" />
+import vue from "@vitejs/plugin-vue"
 import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
 import sdk from "vite-plugin-sdk"
 
 export default defineConfig({
-	plugins: [sdk()],
+	plugins: [sdk(), vue(), dts({ include: "src/**/*.vue" })],
 	// @ts-expect-error Vite 6 issue(?)
 	test: {
 		environment: "jsdom",
