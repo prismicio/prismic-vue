a4cc1a5b88e993cb770bb5180514fcbd3bbe7381
lihbr <lihbr@users.noreply.github.com>
2024-12-04 17:28:06 +0100

    refactor!: <PrismicText />

---
+++ src/components/PrismicText.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+import type { RichTextField } from "@prismicio/client"
+import { asText, isFilled } from "@prismicio/client"
+import { watchEffect } from "vue"
+
+import { devMsg } from "../lib/devMsg"
+
+import type { ComponentOrTagName } from "../types"
+
+/**
+ * Props for `<PrismicText />`.
+ */
+export type PrismicTextProps = {
+	/**
+	 * The Prismic Rich Text field to render.
+	 */
+	field: RichTextField | null | undefined
+
+	/**
+	 * The string value to be rendered when the field is empty. If a fallback is
+	 * not given, `""` (nothing) will be rendered.
+	 */
+	fallback?: string
+
+	/**
+	 * The separator used between blocks.
+	 *
+	 * @defaultValue `" "` (a whitespace)
+	 */
+	separator?: string
+
+	/**
+	 * An HTML tag name, a component, or a functional component used to wrap the
+	 * output.
+	 *
+	 * @defaultValue `"template"` (no wrapper)
+	 */
+	wrapper?: ComponentOrTagName
+}
+
+const props = defineProps<PrismicTextProps>()
+defineOptions({ name: "PrismicText" })
+
+if (typeof process !== "undefined" && process.env.NODE_ENV === "development") {
+	watchEffect(() => {
+		if (typeof props.field === "string") {
+			throw new Error(
+				`[PrismicText] The "field" prop only accepts a Rich Text or Title field's value but was provided a different type of field instead (e.g. a Key Text or Select field). You can resolve this error by rendering the field value inline without <PrismicText>. For more details, see ${devMsg(
+					"prismictext-works-only-with-rich-text-and-title-fields",
+				)}`,
+			)
+		}
+	}, {})
+}
+</script>
+
+<template>
+	<template v-if="isFilled.richText(field) || fallback">
+		<Component v-if="wrapper" :is="wrapper">
+			{{ asText(field) || fallback }}
+		</Component>
+		<template v-else>
+			{{ asText(field) || fallback }}
+		</template>
+	</template>
+</template>
--- test/components-PrismicText.test.ts
+++ test/components-PrismicText.test.ts
@@ -1,77 +1,148 @@
 import { expect, it } from "vitest"
 
-import type { RichTextField } from "@prismicio/client"
+import { type RichTextField, RichTextNodeType } from "@prismicio/client"
 import { mount } from "@vue/test-utils"
 import { markRaw } from "vue"
 
 import { WrapperComponent } from "./__fixtures__/WrapperComponent"
-import { richTextFixture } from "./__fixtures__/richText"
 
-import { PrismicTextImpl } from "../src/components"
+import { PrismicText } from "../src/components"
 
-it("renders rich text field as plain text", () => {
-	const wrapper = mount(PrismicTextImpl, {
-		props: { field: richTextFixture.en },
+it("renders plain text representation", () => {
+	const wrapper = mount(PrismicText, {
+		props: {
+			field: [
+				{
+					type: RichTextNodeType.heading1,
+					text: "Heading 1",
+					spans: [],
+				},
+			],
+		},
 	})
 
-	expect(wrapper.html()).toMatchSnapshot()
+	expect(wrapper.html()).toBe("Heading 1")
 })
 
-it("renders fallback when rich text is empty", () => {
-	const wrapper1 = mount(PrismicTextImpl, {
-		props: { field: [] },
+it("returns null when passed empty field", () => {
+	const wrapper = mount(PrismicText, {
+		props: { field: null },
 	})
 
-	expect(wrapper1.html()).toBe("<div></div>")
+	expect(wrapper.html()).toBe("<!--v-if-->")
+})
 
-	const wrapper2 = mount(PrismicTextImpl, {
-		props: { field: [], fallback: "foo" },
+it("returns fallback when passed empty field", () => {
+	const nullField = mount(PrismicText, {
+		props: { field: null, fallback: "fallback" },
 	})
 
-	expect(wrapper2.html()).toBe("<div>foo</div>")
-
-	const wrapper3 = mount(PrismicTextImpl, {
-		props: { field: null, fallback: "bar" },
+	const undefinedField = mount(PrismicText, {
+		props: { field: undefined, fallback: "fallback" },
 	})
 
-	expect(wrapper3.html()).toBe("<div>bar</div>")
-
-	const wrapper4 = mount(PrismicTextImpl, {
-		props: { field: undefined, fallback: "baz" },
+	const emptyField = mount(PrismicText, {
+		props: { field: [], fallback: "fallback" },
 	})
 
-	expect(wrapper4.html()).toBe("<div>baz</div>")
+	expect(nullField.html()).toBe("fallback")
+	expect(undefinedField.html()).toBe("fallback")
+	expect(emptyField.html()).toBe("fallback")
+})
+
+it("returns fallback when passed empty field with wrapper", () => {
+	const nullField = mount(PrismicText, {
+		props: { field: null, fallback: "fallback", wrapper: "p" },
+	})
+
+	const undefinedField = mount(PrismicText, {
+		props: { field: undefined, fallback: "fallback", wrapper: "p" },
+	})
+
+	const emptyField = mount(PrismicText, {
+		props: { field: [], fallback: "fallback", wrapper: "p" },
+	})
+
+	expect(nullField.html()).toBe("<p>fallback</p>")
+	expect(undefinedField.html()).toBe("<p>fallback</p>")
+	expect(emptyField.html()).toBe("<p>fallback</p>")
 })
 
 it("uses provided wrapper tag", () => {
-	const wrapper = mount(PrismicTextImpl, {
-		props: { field: richTextFixture.en, wrapper: "p" },
+	const wrapper = mount(PrismicText, {
+		props: {
+			field: [
+				{
+					type: RichTextNodeType.heading1,
+					text: "Heading 1",
+					spans: [],
+				},
+			],
+			wrapper: "p",
+		},
 	})
 
-	expect(wrapper.html()).toMatchSnapshot()
+	expect(wrapper.html()).toBe("<p>Heading 1</p>")
 })
 
 it("uses provided wrapper component", () => {
-	const wrapper = mount(PrismicTextImpl, {
-		props: { field: richTextFixture.en, wrapper: markRaw(WrapperComponent) },
+	const wrapper = mount(PrismicText, {
+		props: {
+			field: [
+				{
+					type: RichTextNodeType.heading1,
+					text: "Heading 1",
+					spans: [],
+				},
+			],
+			wrapper: markRaw(WrapperComponent),
+		},
 	})
 
-	expect(wrapper.html()).toMatchSnapshot()
+	expect(wrapper.html()).toBe(`<div class="wrapperComponent">Heading 1</div>`)
+})
+
+it("throws error if passed a string-based field (e.g. Key Text or Select)", () => {
+	// The error is only thrown  in "development".
+	const originalNodeEnv = process.env.NODE_ENV
+	process.env.NODE_ENV = "development"
+
+	expect(() =>
+		mount(PrismicText, {
+			props: {
+				// @ts-expect-error - We are purposely not providing a correct field.
+				field: "not a Rich Text field",
+				wrapper: markRaw(WrapperComponent),
+			},
+		}),
+	).toThrowError(/prismictext-works-only-with-rich-text-and-title-fields/i)
+
+	process.env.NODE_ENV = originalNodeEnv
 })
 
 it("reacts to changes properly", async () => {
-	const wrapper = mount(PrismicTextImpl, {
-		props: { field: richTextFixture.en },
+	const wrapper = mount(PrismicText, {
+		props: {
+			field: [
+				{
+					type: RichTextNodeType.heading1,
+					text: "Heading 1",
+					spans: [],
+				},
+			],
+		},
 	})
 
 	const firstRender = wrapper.html()
 
 	await wrapper.setProps({
-		field: [{ type: "paragraph", text: "foo", spans: [] }] as RichTextField,
+		field: [
+			{ type: RichTextNodeType.paragraph, text: "foo", spans: [] },
+		] as RichTextField,
 	})
 
 	const secondRender = wrapper.html()
 
 	expect(secondRender).not.toBe(firstRender)
-	expect(secondRender).toBe("<div>foo</div>")
+	expect(secondRender).toBe("foo")
 })
