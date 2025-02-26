2904beef84add8ca1411f65f489164a0b3e902f5
Levi Mykel Gable <9385378+levimykel@users.noreply.github.com>
2025-02-26 12:30:23 +0100

    refactor: simplify types

--- src/PrismicTable/types.ts
+++ src/PrismicTable/types.ts
@@ -14,51 +14,21 @@ import type {
 } from "vue"
 
 /**
- * Types enum for TableNodes
- *
- * @see More details: {@link https://prismic.io/docs/table}
- */
-export declare const TableNodeType: {
-	readonly table: "table"
-	readonly thead: "thead"
-	readonly tbody: "tbody"
-	readonly tr: "tr"
-	readonly th: "th"
-	readonly td: "td"
-}
-
-/**
  * A map of Table block types to Vue Components. It is used to render table
  * fields.
  *
  * @see Templating Table fields from Prismic {@link https://prismic.io/docs/table}
  */
-export type VueTableComponents = Partial<
-	Record<keyof typeof TableNodeType, VueTableComponent>
->
-
-export type TAnyNode =
-	| TableField
-	| TableFieldBody
-	| TableFieldBodyRow
-	| TableFieldDataCell
-	| TableFieldHead
-	| TableFieldHeadRow
-	| TableFieldHeaderCell
-
-/**
- * Props for a component rendering nodes from a Prismic table field.
- */
-export type TableComponentProps<TTNode extends TAnyNode = TAnyNode> = {
-	node: TTNode
+export type VueTableComponents = {
+	table?: VueComponent<{ table: TableField<"filled"> }>
+	thead?: VueComponent<{ head: TableFieldHead }>
+	tbody?: VueComponent<{ body: TableFieldBody }>
+	tr?: VueComponent<{ row: TableFieldBodyRow | TableFieldHeadRow }>
+	th?: VueComponent<{ cell: TableFieldHeaderCell }>
+	td?: VueComponent<{ cell: TableFieldDataCell }>
 }
 
-/**
- * A Vue component rendering a node from a Prismic table field.
- *
- * @typeParam TTNode - The type of table node(s) this component handles
- */
-export type VueTableComponent<TTNode extends TAnyNode = TAnyNode> =
+type VueComponent<TProps> =
 	| ReturnType<typeof defineAsyncComponent>
-	| DefineComponent<TableComponentProps<TTNode>>
-	| FunctionalComponent<TableComponentProps<TTNode>>
+	| DefineComponent<TProps>
+	| FunctionalComponent<TProps>
